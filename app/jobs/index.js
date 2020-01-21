8eae2e095c24575ea4c73c1e8ef83538d0c84a90
ekspil <ekspil@yandex.ru>
2020-01-21 11:33:22 +1000

    check notifications

--- app/jobs/index.js
+++ app/jobs/index.js
@@ -25,7 +25,7 @@ const jobs = (injects) => {
                     logger.error(e)
                 })
         })        // Every 10 minutes
-        cron.schedule("*/5 * * * *", () => {
+        cron.schedule("*/1 * * * *", () => {
             checkNotificationsJob()
                 .then(log => logger.info(log))
                 .catch((e) => {

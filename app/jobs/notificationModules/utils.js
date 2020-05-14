6d2e988d76883cf19f034421fa11f4a8dd12e8a5
ekspil <ekspil@yandex.ru>
2020-05-14 14:41:44 +1000

    new notifications

--- app/jobs/notificationModules/utils.js
+++ app/jobs/notificationModules/utils.js
@@ -1,6 +1,5 @@
 
 const fetch = require("node-fetch")
-const logger = require("my-custom-logger")
 
 const notificationTime = {}
 
@@ -39,8 +38,6 @@ const sendTelegram = async (chat, msg) => {
 }
 
 const sendEmail = async (email, msg) => {
-    logger.info(msg)
-    logger.info(email)
     const body = JSON.stringify({email, msg})
     const url = `${process.env.NOTIFICATION_URL}/api/v1/template/EMAIL_MSG`
     const method = "POST"

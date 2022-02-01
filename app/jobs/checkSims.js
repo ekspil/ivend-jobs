d6ab813782449392cf2b0fe5faf8f0c068864dd9
Efremov AV <ekspil@yandex.ru>
2022-02-01 11:28:49 +1000

    fix admin statistic

--- app/jobs/checkSims.js
+++ app/jobs/checkSims.js
@@ -94,7 +94,7 @@ module.exports = (injects) => {
         const fileAddress = path.join(__dirname, "result.txt")
 
         let file = await fs.readFileSync(fileAddress, "utf8")
-        logger.info("file")
+        logger.info(file)
         file = file.split("\r\n")
         file = file.map(item => {
             const arr = item.split(";")
